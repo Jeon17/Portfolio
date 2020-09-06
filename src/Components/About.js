43b254b9b2d6cf0cea6266070c0bb1d61e912cb9
Jeon17 <jthomassocial@gmail.com>
2020-09-06 10:14:12 -0400

    change file content

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ export default class About extends Component {
       <section id="about">
         <div className="row">
           <div className="three columns">
-            <img className="profile-pic" src="./profilepic.jpg.png" alt="" />
+            <img className="profile-pic" src="./images/profilepic.jpg" alt="" />
           </div>
 
           <div className="nine columns main-col">
