8c688743692d756e2c8bdb162724759a901ab556
Jeon17 <jthomassocial@gmail.com>
2020-09-06 12:21:49 -0400

    new change to file

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,24 +4,24 @@ export default class Contact extends Component {
     render() {
         let resumeData = this.props.resumeData;
         return (
-            <section id="contact">
-                <div className="row section-head">
-                    <div className="ten columns">
-                        <p className="lead">
-                            Please contact any of my social accounts!
-              </p>
-                    </div>
+          <section id="contact">
+            <div className="row section-head">
+              <div className="ten columns">
+                <p className="lead">
+                  Please contact any of my social accounts!
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <aside className="eigth columns footer-widgets">
+                <div className="widget">
+                  <h4>
+                    LinkedIn:<br>{resumeData.linkedIn} </br>
+                  </h4>
                 </div>
-                <div className="row">
-                    <aside className="eigth columns footer-widgets">
-                        <div className="widget">
-                            <h4>LinkedIn:
-                  {resumeData.linkedIn}
-                            </h4>
-                        </div>
-                    </aside>
-                </div>
-            </section>
+              </aside>
+            </div>
+          </section>
         );
     }
 }
